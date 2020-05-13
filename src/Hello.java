3b07f54c84fac754daedbd36e7194acdaea96801
B-14 <B-14@DESKTOP-VKAK17S>
2020-05-13 10:40:38 +0900

    test

--- src/Hello.java
+++
@@ -1,8 +0,0 @@
-
-public class Hello {
-	public static void main(String[] args) {
-		System.out.println("Hello");
-		System.out.println("Very good");
-		System.out.println("I'll be a good programmer");
-	}
-}
