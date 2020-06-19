900f4aa4665b2e4514d0728f68cb1af0ec0d58b1
Happydj <56625382+PARKDONGJAE@users.noreply.github.com>
2020-06-19 20:38:59 +0900

    update

---
+++ src/baekjun/Main.java
@@ -0,0 +1,22 @@
+﻿package baekjun;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		short a = scan.nextShort();
+		short b = scan.nextShort();
+		if (1 <=a && a <= 10000 && 1 <= b && b <= 10000) {
+			System.out.println(a + b);
+			System.out.println(a - b);
+			System.out.println(a * b);
+			System.out.println(a / b);
+			System.out.println(a % b);
+		}
+
+	}
+
+}
--- src/programmers/sosu.java
+++ src/programmers/sosu.java
@@ -1,29 +1,31 @@
 package programmers;
+
 class Solution {
-    public int solution(int n) {
-        int answer = 0;
-        for (int i = 2; i<=n;i++){
-            if(n<2&&n>1000000) {
-        		break;
-        	}
-            int count =0;
-            for (int j= 2; j<=n; j++){
-               
-                if(i%j!=0){
-                    count++;
-                } 
-                if(i<=j){
-                    break;
-                }
-            }
-            if((count+2)==i){
-                answer++;
-            }
-        }
-        return answer;
-        
-    }
+	public int solution(int n) {
+		int answer = 0;
+		for (int i = 2; i <= n; i++) {
+			if (n < 2 && n > 1000000) {
+				break;
+			}
+			int count = 0;
+			for (int j = 2; j <= n; j++) {
+
+				if (i % j != 0) {
+					count++;
+				}
+				if (i <= j) {
+					break;
+				}
+			}
+			if ((count + 2) == i) {
+				answer++;
+			}
+		}
+		return answer;
+
+	}
 }
+
 public class sosu {
 
 	public static void main(String[] args) {
